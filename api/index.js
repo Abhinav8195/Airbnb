815a7907221961d4b8a6611d0344a2f3c3703956
Abhinav8195 <149519597+Abhinav8195@users.noreply.github.com>
2024-07-24 11:30:27 +0530

    first commit

--- api/index.js
+++ api/index.js
@@ -5,31 +5,48 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const User = require('./models/User');
 const bcrypt = require('bcrypt');
-const cookieparser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
-const imagedownloader = require('image-downloader')
-const secret = bcrypt.genSaltSync(10);
-const jwtsecret = 'asdfghjkl';
-const multer = require('multer')
-const fs = require('fs')
+const imagedownloader = require('image-downloader');
+const multer = require('multer');
+const fs = require('fs');
 const Place = require('./models/Place');
-const Booking = require('./models/Bookings')
+const Booking = require('./models/Bookings');
 
+// Load environment variables
+dotenv.config();
+
+// MongoDB connection setup
+let cachedDb = null;
+
+async function connectToDatabase() {
+  if (cachedDb) {
+    return cachedDb;
+  }
+
+  const db = await mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  cachedDb = db;
+  return cachedDb;
+}
+
+// Middleware
 app.use(express.json());
-app.use(cookieparser());
+app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.use(cors({
-    credentials: true,
-    origin: 'http://localhost:5173'
+  credentials: true,
+  origin: 'http://localhost:5173'
 }));
-dotenv.config();
-mongoose.connect(process.env.MONGO_URL);
 
-
-function getUserDataFromToken(req) {
+// Helper function to get user data from token
+async function getUserDataFromToken(req) {
   return new Promise((resolve, reject) => {
-    jwt.verify(req.cookies.token, jwtsecret, {}, (err, userData) => {
+    jwt.verify(req.cookies.token, process.env.JWT_SECRET, {}, (err, userData) => {
       if (err) {
         reject(err);
       } else {
@@ -38,8 +55,10 @@ function getUserDataFromToken(req) {
     });
   });
 }
+
+// Routes
 app.get('/test', (req, res) => {
-    res.json('test ok');
+  res.json('test ok');
 });
 
 app.post('/register', async (req, res) => {
@@ -47,19 +66,17 @@ app.post('/register', async (req, res) => {
   if (!name || !email || !password) {
     return res.status(400).json({ error: 'Please provide all required fields.' });
   }
-  // Add more specific validations for email and password format
   try {
     const user = await User.create({
-      name: name,
-      email: email,
-      password: bcrypt.hashSync(password, secret)
+      name,
+      email,
+      password: bcrypt.hashSync(password, process.env.BCRYPT_SALT)
     });
     res.json({ user });
   } catch (error) {
     res.status(422).json({ error: 'Registration failed. Please try again.' });
   }
 });
-
 
 app.post('/login', async (req, res) => {
   const { email, password } = req.body;
@@ -75,7 +92,7 @@ app.post('/login', async (req, res) => {
     jwt.sign({
       email: user.email,
       id: user._id,
-    }, jwtsecret, {}, (err, token) => {
+    }, process.env.JWT_SECRET, {}, (err, token) => {
       if (err) throw err;
       res.cookie('token', token).json(user);
     });
@@ -85,80 +102,99 @@ app.post('/login', async (req, res) => {
   }
 });
 
-
 app.get('/profile', async (req, res) => {
   const { token } = req.cookies;
   try {
-      if (!token) {
-          throw new Error('Authentication token not found');
-      }
+    if (!token) {
+      throw new Error('Authentication token not found');
+    }
 
-      jwt.verify(token, jwtsecret, {}, async (err, userData) => {
-          if (err) {
-              throw new Error('Invalid or expired token');
-          }
-          const user = await User.findById(userData.id);
-          if (!user) {
-              throw new Error('User not found');
-          }
-          res.json({ name: user.name, email: user.email, _id: user._id });
-      });
+    jwt.verify(token, process.env.JWT_SECRET, {}, async (err, userData) => {
+      if (err) {
+        throw new Error('Invalid or expired token');
+      }
+      const user = await User.findById(userData.id);
+      if (!user) {
+        throw new Error('User not found');
+      }
+      res.json({ name: user.name, email: user.email, _id: user._id });
+    });
   } catch (error) {
-      console.error('Error in /profile route:', error);
-      res.status(401).json({ error: 'Authentication failed' });
+    console.error('Error in /profile route:', error);
+    res.status(401).json({ error: 'Authentication failed' });
   }
 });
 
-app.post('/logout', async (req, res) => {
-    res.cookie('token', '', { maxAge: 1 }).json(true);
-  });
+app.post('/logout', (req, res) => {
+  res.cookie('token', '', { maxAge: 1 }).json(true);
+});
 
-  const photosmulter = multer({ dest: 'uploads' });
+const photosMulter = multer({ dest: 'uploads/' });
 
-  app.post('/upload-by-link', async (req, res) => {
-    const { link } = req.body;
-    const newName = 'photo' + Date.now() + '.jpg';
-    try {
-      await imagedownloader.image({
-        url: link,
-        dest: __dirname + '/uploads/' + newName,
-      });
-      console.log('File downloaded:', newName); // Log the file name
-      res.json(newName);
-    } catch (error) {
-      console.error('Error downloading image:', error);
-      res.status(500).send('Error downloading image');
+app.post('/upload-by-link', async (req, res) => {
+  const { link } = req.body;
+  const newName = 'photo' + Date.now() + '.jpg';
+  try {
+    await imagedownloader.image({
+      url: link,
+      dest: __dirname + '/uploads/' + newName,
+    });
+    console.log('File downloaded:', newName);
+    res.json(newName);
+  } catch (error) {
+    console.error('Error downloading image:', error);
+    res.status(500).send('Error downloading image');
+  }
+});
+
+app.post('/upload', photosMulter.array('photos', 100), async (req, res) => {
+  const uploadedFiles = [];
+  try {
+    for (let i = 0; i < req.files.length; i++) {
+      const { path, originalname } = req.files[i];
+      const parts = originalname.split('.');
+      const ext = parts[parts.length - 1];
+      const newName = 'photo' + Date.now() + '.' + ext;
+      const newPath = __dirname + '/uploads/' + newName;
+      fs.renameSync(path, newPath);
+      uploadedFiles.push(newName);
     }
-  });
-  
-  
-  app.post('/upload', photosmulter.array('photos', 100), async (req, res) => {
-    const uploadedFiles = [];
-    try {
-      for (let i = 0; i < req.files.length; i++) {
-        const { path, originalname } = req.files[i];
-        const parts = originalname.split('.');
-        const ext = parts[parts.length - 1];
-        const newName = 'photo' + Date.now() + '.' + ext; // Generate a unique filename
-            const newPath = __dirname + '/uploads/' + newName; // Construct the new path
-            fs.renameSync(path, newPath); // Move the file to the uploads folder with the new name
-            uploadedFiles.push(newName);
-      }
-      console.log('Uploaded files:', uploadedFiles); // Log the uploaded files
-      res.json(uploadedFiles);
-    } catch (error) {
-      console.error('Error uploading files:', error);
-      res.status(500).send('Error uploading files');
+    console.log('Uploaded files:', uploadedFiles);
+    res.json(uploadedFiles);
+  } catch (error) {
+    console.error('Error uploading files:', error);
+    res.status(500).send('Error uploading files');
+  }
+});
+
+app.post('/places', async (req, res) => {
+  const { token } = req.cookies;
+  const {
+    title,
+    address,
+    addedPhotos,
+    description,
+    perks,
+    extraInfo,
+    checkIn,
+    checkOut,
+    maxGuests,
+    price
+  } = req.body;
+
+  console.log('Request body:', req.body);
+
+  try {
+    const userData = await getUserDataFromToken(req);
+    if (!userData) {
+      return res.status(401).send('Unauthorized');
     }
-  });
-  
-  
-  app.post('/places', (req, res) => {
-    const { token } = req.cookies;
-    const {
+
+    const placeDoc = await Place.create({
+      owner: userData.id,
       title,
       address,
-      addedPhotos, 
+      photos: addedPhotos,
       description,
       perks,
       extraInfo,
@@ -166,60 +202,63 @@ app.post('/logout', async (req, res) => {
       checkOut,
       maxGuests,
       price
-    } = req.body;
-  
-    console.log('Request body:', req.body); 
-  
-    jwt.verify(token, jwtsecret, {}, async (err, userData) => {
-      if (err) {
-        console.error('JWT verification error:', err);
-        res.status(401).send('Unauthorized');
-        return;
-      }
-  
-      try {
-        const placeDoc = await Place.create({
-          owner: userData.id,
-          title,
-          address,
-          photos: addedPhotos, 
-          description,
-          perks,
-          extraInfo,
-          checkIn,
-          checkOut,
-          maxGuests,
-          price
-        });
-        res.json(placeDoc);
-      } catch (error) {
-        console.error('Error creating place:', error);
-        res.status(500).send('Server error');
-      }
     });
-  });
-  
-  
-  
 
-  app.get('/user-places',async(req,res)=>{
+    res.json(placeDoc);
+  } catch (error) {
+    console.error('Error creating place:', error);
+    res.status(500).send('Server error');
+  }
+});
+
+app.get('/user-places', async (req, res) => {
+  try {
     const { token } = req.cookies;
-    jwt.verify(token, jwtsecret, {}, async (err, userData) => {
-        const {id} =userData;
-        res.json(await Place.find({owner:id}))
-    })
-  });
-  app.get('/places/:id',async(req,res)=>{
-    const {id} =req.params;
-    res.json(await Place.findById(id))
-  })
-  app.put('/places',async(req,res)=>{
-    const { token } = req.cookies;
-    const {
-        id,
+    const userData = await getUserDataFromToken(req);
+    const places = await Place.find({ owner: userData.id });
+    res.json(places);
+  } catch (error) {
+    console.error('Error fetching user places:', error);
+    res.status(500).send('Server error');
+  }
+});
+
+app.get('/places/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const place = await Place.findById(id);
+    res.json(place);
+  } catch (error) {
+    console.error('Error fetching place:', error);
+    res.status(500).send('Server error');
+  }
+});
+
+app.put('/places', async (req, res) => {
+  const { token } = req.cookies;
+  const {
+    id,
+    title,
+    address,
+    addedPhotos,
+    description,
+    perks,
+    extraInfo,
+    checkIn,
+    checkOut,
+    maxGuests,
+    price
+  } = req.body;
+
+  try {
+    const userData = await getUserDataFromToken(req);
+    const placeDoc = await Place.findById(id);
+
+    if (userData.id === placeDoc.owner.toString()) {
+      placeDoc.set({
         title,
         address,
-        addedPhotos, 
+        photos: addedPhotos,
         description,
         perks,
         extraInfo,
@@ -227,33 +266,26 @@ app.post('/logout', async (req, res) => {
         checkOut,
         maxGuests,
         price
-      } = req.body;
-      
-      jwt.verify(token, jwtsecret, {}, async (err, userData) => {
-        if(err) throw err;
-        const placeDoc = await Place.findById(id)
-        if(userData.id === placeDoc.owner.toString()){
-            placeDoc.set({
-               
-          title,
-          address,
-          photos: addedPhotos, 
-          description,
-          perks,
-          extraInfo,
-          checkIn,
-          checkOut,
-          maxGuests,
-          price
-            })
-          await   placeDoc.save()
-            res.json('ok')
-        }
-      })
-  });
+      });
+      await placeDoc.save();
+      res.json('ok');
+    } else {
+      res.status(403).send('Forbidden');
+    }
+  } catch (error) {
+    console.error('Error updating place:', error);
+    res.status(500).send('Server error');
+  }
+});
 
-app.get('/places',async(req,res)=>{
-  res.json (await Place.find({}))
+app.get('/places', async (req, res) => {
+  try {
+    const places = await Place.find({});
+    res.json(places);
+  } catch (error) {
+    console.error('Error fetching places:', error);
+    res.status(500).send('Server error');
+  }
 });
 
 app.post('/booking', async (req, res) => {
@@ -274,7 +306,7 @@ app.post('/booking', async (req, res) => {
       mobile,
       user: userData.id
     });
-    
+
     res.json(booking);
   } catch (error) {
     console.error('Error occurred while creating booking:', error);
@@ -282,12 +314,31 @@ app.post('/booking', async (req, res) => {
   }
 });
 
-
-
-
 app.get('/singlebookings', async (req, res) => {
-  const user = await getUserDataFromToken(req);
-  res.json(await Booking.find({ user: user.id }).populate('place'));
+  try {
+    const user = await getUserDataFromToken(req);
+    const bookings = await Booking.find({ user: user.id }).populate('place');
+    res.json(bookings);
+  } catch (error) {
+    console.error('Error fetching user bookings:', error);
+    res.status(500).send('Server error');
+  }
 });
 
-app.listen(4000);
+app.get('/bookings/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const booking = await Booking.findById(id).populate('place');
+    res.json(booking);
+  } catch (error) {
+    console.error('Error fetching booking:', error);
+    res.status(500).send('Server error');
+  }
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
